ee20393292f651e21f59b499fe1c8df6a678505b
Dongkwan Kim <todoaskit@gmail.com>
2020-06-16 17:33:44 +0900

    Implement Tensor struct

--- code/prob1/convolution.c
+++ code/prob1/convolution.c
@@ -3,7 +3,13 @@
 #include <stdlib.h>
 
 
-float* get_tensor(char* file_name) {
+struct Tensor {
+    int shape[4];
+    float* vector;
+};
+
+
+struct Tensor get_tensor(char* file_name) {
     FILE *f;
     if ((f = fopen(file_name, "rb")) == NULL) {
         printf("Error: file opening in %s", file_name);
@@ -12,18 +18,27 @@ float* get_tensor(char* file_name) {
     fseek(f, 0, SEEK_END);
     int file_sz = ftell(f);
     fseek(f, 0, SEEK_SET);
-    float* tensor = (float *) malloc(file_sz);
-    fread(tensor, file_sz, 1, f);
+
+    int shape[4] = {-1, -1, -1, -1};
+    float* vector = (float *) malloc(file_sz - 16);  // 16 for shape
+
+    fread(shape, 4, 4, f);
+    fread(vector, file_sz - 16, 1, f);
+
     printf("Read file: %d of %s\n", file_sz, file_name);
+
+    struct Tensor tensor;
+    tensor.vector = vector;
+    memcpy(tensor.shape, shape, 16);
     return tensor;
 }
 
 int main (int argc, char* argv[]) {
 
-    float* tensor_in = get_tensor(argv[1]);
-    float* tensor_ke = get_tensor(argv[2]);
+    struct Tensor tensor_in = get_tensor(argv[1]);
+    struct Tensor tensor_ke = get_tensor(argv[2]);
 
-    free(tensor_in);
-    free(tensor_ke);
+    free(tensor_in.vector);
+    free(tensor_ke.vector);
     return 0;
 }
